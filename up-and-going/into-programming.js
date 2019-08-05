4799b6d3513bfd07919bd990937711a03870f597
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2019-08-05 20:06:22 +0200

    Add executing a program notes

--- up-and-going/into-programming.js
+++ up-and-going/into-programming.js
@@ -1,6 +1,8 @@
 let b = 0;
 let a = 0
 
+// ------------------------------------------------------------------------------
+
 // Statements
 // Group of words, numbers/operators that perform a task
 
@@ -10,6 +12,8 @@ a = b * 2;
 // 2 => literal value
 // =, *  => operators, perform actions such as assignment or mathematical operations
 // a = b * 2 => Get the current value of b, multiply by 2, store in a
+
+// ------------------------------------------------------------------------------
 
 // Expressions
 // Reference to a variable/value, or set of the same combined with operators
@@ -23,4 +27,10 @@ a = b * 2;
 
 b * 2 // expression statement (more useful in call expression statement)
 
+// ------------------------------------------------------------------------------
+
 // Executing a program
+// Done by interpreter or a compiler to translate the code for computer
+// Interpreting the code => top to bottom, line by line
+// Compiling the code => translation is done ahead of time, compiled instructions ready to go
+// JS engine compiles on the fly & runs the compiled code
