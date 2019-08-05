7b4c3288ebc9a1241a3eba19c494d53506854d3e
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2019-08-05 20:41:31 +0200

    Add operator notes

--- up-and-going/into-programming.js
+++ up-and-going/into-programming.js
@@ -1,5 +1,6 @@
 let b = 0;
-let a = 0
+let a = 0;
+const obj = { a: 'a' };
 
 // ------------------------------------------------------------------------------
 
@@ -8,10 +9,10 @@ let a = 0
 
 a = b * 2;
 
-// a, b => variables
-// 2 => literal value
-// =, *  => operators, perform actions such as assignment or mathematical operations
-// a = b * 2 => Get the current value of b, multiply by 2, store in a
+// a, b -> variables
+// 2 -> literal value
+// =, *  -> operators, perform actions such as assignment or mathematical operations
+// a = b * 2 -> Get the current value of b, multiply by 2, store in a
 
 // ------------------------------------------------------------------------------
 
@@ -20,10 +21,10 @@ a = b * 2;
 
 a = b * 2;
 
-// 2 => lieteral value expression
-// b => variable expression (get the value of b)
-// b * 2 => arithmetic expression (do the multiplication by 2 on value from b)
-// a = b * 2 => assignment expression (assign the result of b * 2 to a)
+// 2 -> lieteral value expression
+// b -> variable expression (get the value of b)
+// b * 2 -> arithmetic expression (do the multiplication by 2 on value from b)
+// a = b * 2 -> assignment expression (assign the result of b * 2 to a)
 
 b * 2 // expression statement (more useful in call expression statement)
 
@@ -31,6 +32,26 @@ b * 2 // expression statement (more useful in call expression statement)
 
 // Executing a program
 // Done by interpreter or a compiler to translate the code for computer
-// Interpreting the code => top to bottom, line by line
-// Compiling the code => translation is done ahead of time, compiled instructions ready to go
+// Interpreting the code -> top to bottom, line by line
+// Compiling the code -> translation is done ahead of time, compiled instructions ready to go
 // JS engine compiles on the fly & runs the compiled code
+
+// ------------------------------------------------------------------------------
+
+// Operators
+// Actions that perform on variables and values
+
+// * -> mathematic multiplication
+// = -> (target variable) = (source value), used for assignment, source value is calculated first, and then put into target variable
+
+a = 2; // assign 2 value to a variable
+b = a + 1; // get the value of a, add 1 to it, assign it to b
+
+a = 2 // assignment operator
+a + 2; a - 2; a / 2; a * 2; // Math operators
+a += 2; a -= 2; a *= 2; a /= 2; // Compound operators (Math operator + assignment operator)
+a++; a--; // Increment/Decrement operators
+obj.a; obj["a"]; // Object property access (., [""])
+a == 2; a === 2; a !== 2; a != 2; // Equality operators
+a > 2; a < 2; a <= 2; a >= 2; // Comparison operators
+a && b; a || b; // Logical operators, used to express compound contitionals
