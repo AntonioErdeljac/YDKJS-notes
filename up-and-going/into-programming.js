7a1f148020c9a4b71253978148285339af283462
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2019-08-05 20:01:30 +0200

    initial commit

---
+++ up-and-going/into-programming.js
@@ -0,0 +1,26 @@
+let b = 0;
+let a = 0
+
+// Statements
+// Group of words, numbers/operators that perform a task
+
+a = b * 2;
+
+// a, b => variables
+// 2 => literal value
+// =, *  => operators, perform actions such as assignment or mathematical operations
+// a = b * 2 => Get the current value of b, multiply by 2, store in a
+
+// Expressions
+// Reference to a variable/value, or set of the same combined with operators
+
+a = b * 2;
+
+// 2 => lieteral value expression
+// b => variable expression (get the value of b)
+// b * 2 => arithmetic expression (do the multiplication by 2 on value from b)
+// a = b * 2 => assignment expression (assign the result of b * 2 to a)
+
+b * 2 // expression statement (more useful in call expression statement)
+
+// Executing a program
